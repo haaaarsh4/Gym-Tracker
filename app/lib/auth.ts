03ef70faaad514e193857e66e453678d4986bbb2
haaaarsh4 <harshupadhyay2004@gmail.com>
2025-06-24 22:40:16 -0400

    hopefully last change

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -6,6 +6,7 @@ import prisma from "./db"
 import bcrypt from "bcryptjs"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    trustHost: true,
     adapter: PrismaAdapter(prisma),
     session: {
         strategy: "jwt"
